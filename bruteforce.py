68829d72e7368a84bb46d03faf40fef6f90e8823
pierreparajua <pierre.parajua34gmail.com>
2022-03-12 10:52:16 +0100

    Feat optimize.py Feat optimize_10.py Feat optimize_100.py optimize.py --> the fastest solution but not precise. optimize_10.py --> fast solution and precise solution optimize_100.py --> the slowest solution but perfect results

--- bruteforce.py
+++ bruteforce.py
@@ -1,4 +1,6 @@
-import csv
+import pandas as pd
+
+import time
 import itertools
 import operator
 from dataclasses import dataclass
@@ -9,70 +11,79 @@ DATA_DIR = Path(__file__).resolve().parent / "data"
 DATA_BRUTE = DATA_DIR / "data_brute.csv"
 DATASET1 = DATA_DIR / "dataset1_Python+P7.csv"
 DATASET2 = DATA_DIR / "dataset2_Python+P7.csv"
-test = DATA_DIR / "test.csv"
-MAX_MONEY = 15
+
+MAX_MONEY = 500
 
 
 @dataclass
 class Action:
     name: str
-    price: float
-    profit: float
-
-    def buy(self) -> float:
-        """
-        Compute the gain for the purchase according to number of action
-        :return: gain for the purchase
-        """
-        return self.price * self.profit
+    price: int
+    profit: int
+    gain: int
 
 
-def load_data(csv_file: Path) -> list[Action]:
+def load_data(csv_file: str) -> list:
     """
-    load datas from csv file and return a list of "Action"
-    :param csv_file: csv file to load
-    :return: List of objet Action
+    Create a dataframe from csv file.
+    Delete value <= 0.
+    Create a new columns "gains" and sort the dataframe according to "gain".
+    :param csv_file:csv file to load
+    :return:A list of objet action.
     """
-    values = []
-    with open(DATA_DIR / csv_file, 'r', newline='') as f:
-        datas = csv.reader(f)
-        for row in datas:
-            if row[0] != "name":
-                value = Action(name=row[0], price=float(row[1]), profit=float(row[2]) / 100)
-                values.append(value)
-        return values
+    df = pd.read_csv(csv_file, sep=",")
+    df.drop(df[df['price'] <= 0].index, inplace=True)
+    df.drop(df[df['profit'] <= 0].index, inplace=True)
+    df['gain'] = df['price'] * df['profit']
+    df_sorted = df.sort_values(by="gain", ascending=False)
+    actions = [Action(name=action[0], price=action[1], profit=action[2],
+                      gain=action[3]) for action in df_sorted.values]
+    return actions
 
 
-def force_brute(datas):
+def force_brute(datas: list) -> list:
     """
-    Compute all combinaisons, add each combinaison to the list "all_combinaisons" if the total cost <= 500€,
+    Compute all combinations, add each combinations to the list "all_combinations" if the total cost <= MAX_MONEY,
     and to finish, sort this list by profit and return it
-    :param datas: datas to compute
-    :return: sorted list by profit
+    :param datas: Datas to test.
+    :return: Sorted list by profit.
     """
-    all_combinaisons = []
-    for L in range(0, len(datas) + 1):
-        for combinaison in itertools.combinations(datas, L):
-            nbr_action_bought = len(combinaison)
+    all_combinations = []
+    for nbr_action in range(0, len(datas) + 1):
+        print(nbr_action)
+        for combination in itertools.combinations(datas, nbr_action):
+            nbr_action_bought = len(combination)
             if nbr_action_bought != 0:
-                combinaisons = []
+                combinations = []
                 total_cost = 0
                 total_return = 0
                 for i in range(nbr_action_bought):
-                    profit_by_action = combinaison[i].buy()
-                    combinaisons.append(combinaison[i].name)
+                    profit_by_action = combination[i].gain
+                    combinations.append(combination[i].name)
                     total_return += profit_by_action
-                    total_cost += combinaison[i].price
+                    total_cost += combination[i].price
                 if total_cost <= MAX_MONEY:
-                    all_combinaisons.append([combinaisons, total_cost, total_return])
-    return sorted(all_combinaisons, key=operator.itemgetter(2), reverse=True)
+                    all_combinations.append([combinations, total_cost, total_return])
+    return sorted(all_combinations, key=operator.itemgetter(2), reverse=True)
+
+
+def display_result(results: list):
+    """ Display the results"""
+    print("\nRESULTS: ")
+    for action in results[0]:
+        print(action)
+    print(f"Total cost: {results[1]}")
+    print(f"Total return: {results[2]/100}")
+
+
+def main(csv_file):
+    best_combination = force_brute(load_data(csv_file))[0]
+    display_result(best_combination)
 
 
 if __name__ == "__main__":
-    print("Work in progress ...")
-    best_combinaison = force_brute(load_data(test))[0]
-    print("\nRESULTS: ")
-    for action in best_combinaison[0]:
-        print(action)
-    print(f"Total cost: {best_combinaison[1]}")
-    print(f"Total return: {best_combinaison[2]}")
+    start = time.perf_counter()
+    main(DATASET2)
+    end = time.perf_counter()
+    elapsed = end - start
+    print(f'Temps d\'exécution : {elapsed}s')
--- optimize_100.py
+++ optimize_100.py
@@ -1,8 +1,10 @@
 import pandas as pd
+import numpy as np
 
 import time
 from dataclasses import dataclass
 from pathlib import Path
+
 
 DATA_DIR = Path(__file__).resolve().parent / "data"
 
@@ -10,7 +12,7 @@ DATA_BRUTE = DATA_DIR / "data_brute.csv"
 DATASET1 = DATA_DIR / "dataset1_Python+P7.csv"
 DATASET2 = DATA_DIR / "dataset2_Python+P7.csv"
 
-MAX_MONEY = 500
+MAX_MONEY = 50000
 
 
 @dataclass
@@ -25,6 +27,7 @@ def load_data(csv_file: str) -> list:
     """
        Create a dataframe from csv file.
        Delete value <= 0.
+       Modifie "price" and "gain" to obtain only integer
        Create a new columns "gains" and sort the dataframe according to "gain".
        :param csv_file:csv file to load
        :return:A list of objet action.
@@ -32,28 +35,39 @@ def load_data(csv_file: str) -> list:
     df = pd.read_csv(csv_file, sep=",")
     df.drop(df[df['price'] <= 0].index, inplace=True)  # supprime les valeurs < 0
     df.drop(df[df['profit'] <= 0].index, inplace=True)
-    df['gain'] = df['price'] * df['profit']
-    df_sorted = df.sort_values(by="gain", ascending=False)
+    df['price'] = (df['price'] * 100).map(int)
+    df['gain'] = df['price'] * (df['profit'] * 100).map(int)
     actions = [Action(name=action[0], price=action[1], profit=action[2],
-                      gain=action[3]) for action in df_sorted.values]  # Crée une liste d ' objet Action
+                      gain=action[3]) for action in df.values]  # Crée une liste d ' objet Action
     return actions
 
 
 def algo_optimize(datas: list) -> list:
     """
-    Select all actions possible with the best "gain".
+    Create a matrix (nbr_action * MAX_MONEY).
+    Fill in the matrix box by box to find the best combination of actions.
     :param datas: Datas to test.
     :return: A list with the selected actions.
     """
-    actions_selected = []
-    remain_money = MAX_MONEY
-    while remain_money > min(action.price for action in datas):
-        for data in datas:
-            if remain_money - data.price >= 0:
-                actions_selected.append(data)
-                remain_money = remain_money - data.price
-                datas.remove(data)
-    return actions_selected
+    matrice = np.zeros((len(datas) + 1, MAX_MONEY + 1), dtype=int)  # Crée un matrice rempli de zéro (21 * 501)
+    for r in range(1, len(datas) + 1):  # Boucle sur les lignes
+        for c in range(1, MAX_MONEY + 1):  # Boucle sur les colonnes
+            if datas[r - 1].price <= c:
+                matrice[r][c] = max(datas[r - 1].gain + matrice[r - 1][c - datas[r - 1].price],
+                                    matrice[r - 1][c])
+            else:
+                matrice[r][c] = matrice[r - 1][c]
+
+    money = MAX_MONEY
+    n = len(datas)
+    selection = []
+    while money > 0 and n >= 0:
+        action = datas[n - 1]
+        if matrice[n][money] == matrice[n - 1][money - action.price] + action.gain:
+            selection.append(action)
+            money -= action.price
+        n -= 1
+    return selection
 
 
 def display_result(best_actions: list):
@@ -62,10 +76,10 @@ def display_result(best_actions: list):
     tot_return = 0
     for action in best_actions:
         print(action.name)
-        tot += action.price
-        tot_return += action.gain / 100
+        tot += action.price / 100
+        tot_return += action.gain / 1000000
     print(tot)
-    print(tot_return)
+    print(round(tot_return, 2))
 
 
 def main(csv_file):
@@ -75,7 +89,7 @@ def main(csv_file):
 
 if __name__ == "__main__":
     start = time.perf_counter()
-    main(DATA_BRUTE)
+    main(DATASET2)
     end = time.perf_counter()
     elapsed = end - start
     print(f'Temps d\'exécution : {elapsed}s')
